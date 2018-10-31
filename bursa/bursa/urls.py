d5c9d360ddcbe8ea87a3e71a7973f5f46979a28e
arseno feri alzahabi <arsenoferi@hotmail.com>
2018-10-31 16:04:18 +0700

    edit by seno 2

--- bursa/bursa/urls.py
+++ bursa/bursa/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contoh',include('contoh.urls'))
+    path('contoh/',include('contoh.urls')),
 ]
--- bursa/contoh/views.py
+++ bursa/contoh/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ",".join([q.question_text for q in lastest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("you are looking at question %s." %question_id)
+
+def results(request, question_id):
+    response = "you are looking at question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you are voting on question %s." %question_id)
+
